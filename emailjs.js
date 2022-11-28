b94867ef8a45ad27a6b70de8b0b170023511ebf2
Olammyinc <119272402+Olammyinc@users.noreply.github.com>
2022-11-28 04:09:10 +0100

    Create emailjs.js

---
+++ emailjs.js
@@ -0,0 +1,47 @@
+/**sendmail*/
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_v66b9lr";
+    const templateID = "template_sm224aw";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("subject").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+      })
+      .catch(err=>console.log(err));
+  }
+  
+  $(document).ready(function() {
+      $(document).on('submit', '#contactform', function() {
+        // do your things
+       return false;
+       });
+  });
+  
+  /**recaptcha verification*/
+  document.getElementById("contactform").addEventListener("submit",function(evt)
+    {
+    
+    var response = grecaptcha.getResponse();
+    if(response.length == 0) 
+    { 
+      //reCaptcha not verified
+      alert("please verify you are humann!"); 
+     evt.preventDefault();
+     return false;
+    }
+    //captcha verified
+    //do the rest of your validations here
+    
+  });
