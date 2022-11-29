7e4644e034c03c89deb1fb74e0a202df787cc439
Olammyinc <119272402+Olammyinc@users.noreply.github.com>
2022-11-29 11:43:19 +0100

    Update emailjs.js

--- emailjs.js
+++ emailjs.js
@@ -5,6 +5,7 @@ function sendMail() {
       email: document.getElementById("email").value,
       subject: document.getElementById("subject").value,
       message: document.getElementById("message").value,
+      'g-recaptcha-response': document.getElementByClassName("g-recaptcha").value,
     };
   
     const serviceID = "service_v66b9lr";
@@ -16,6 +17,7 @@ function sendMail() {
           document.getElementById("email").value = "";
           document.getElementById("subject").value = "";
           document.getElementById("message").value = "";
+          document.getElementByClassName("g-recaptcha").value = "";
           console.log(res);
           alert("Your message sent successfully!!")
       })
