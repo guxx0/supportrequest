1915ec20217aee9174ee3874946c83c9c31bd5af
guxx0 <109503659+guxx0@users.noreply.github.com>
2024-03-28 02:15:13 -0700

    emailjs.js

--- emailjs.js
+++ emailjs.js
@@ -5,7 +5,6 @@ function sendMail() {
       email: document.getElementById("email").value,
       subject: document.getElementById("subject").value,
       message: document.getElementById("message").value,
-      'g-recaptcha-response': document.getElementByClassName("g-recaptcha").value,
     };
   
     const serviceID = "service_v66b9lr";
@@ -17,7 +16,6 @@ function sendMail() {
           document.getElementById("email").value = "";
           document.getElementById("subject").value = "";
           document.getElementById("message").value = "";
-          document.getElementByClassName("g-recaptcha").value = "";
           console.log(res);
           alert("Your message sent successfully!!")
       })
@@ -29,21 +27,4 @@ function sendMail() {
         // do your things
        return false;
        });
-  });
-  
-  /**recaptcha verification*/
-  document.getElementById("contactform").addEventListener("submit",function(evt)
-    {
-    
-    var response = grecaptcha.getResponse();
-    if(response.length == 0) 
-    { 
-      //reCaptcha not verified
-      alert("please verify you are humann!"); 
-     evt.preventDefault();
-     return false;
-    }
-    //captcha verified
-    //do the rest of your validations here
-    
   });
